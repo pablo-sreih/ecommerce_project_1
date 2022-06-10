f58c17b6b0531f9d878a4729e262673beb32191a
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-10 09:54:27 +0300

    Added Soft Deletes to Tables

--- backend/laravel/app/Models/User.php
+++ backend/laravel/app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
