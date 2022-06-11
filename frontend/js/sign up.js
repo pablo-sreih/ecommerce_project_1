63908a8c8def1db1935c862040838dda6a8dcc03
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-12 01:09:57 +0300

    Added Link to Sign Up

---
+++ frontend/js/sign up.js
@@ -0,0 +1,20 @@
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+document.getElementById("signup-btn").addEventListener('click', send);
+function send(){
+    let data = new FormData;
+    data.append('name', name.value);
+    data.append('email', email.value);
+    data.append('password', password);
+
+    axios({
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/register',
+        data: data,
+    })
+    .then(function (response){
+        console.log(response.data);
+    })
+}
