4e16e97a1e95328985a6d9400f21da904b16e4d9
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-12 23:53:53 +0300

    Added Get Items By Category Frontend

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,6 +12,7 @@ axios({
     for (var i=0; i < response.data["category"].length; i++){
         var z = document.createElement('option');
         z.setAttribute("value", response.data["category"][i]["id"]);
+        z.id = "abc";
         z.innerHTML = response.data["category"][i]["name"];
         categories.appendChild(z);
     }
@@ -39,11 +40,61 @@ axios({
     }
 })
 
-{/* <div id="item-container">
-    <div id="item">
-        <img src="../assets/images/items/psheadset.jpg" alt="">
-        <button>❤</button>
-        <label id="item-title">Playstation Headset</label>
-        <label id="price">170$</label>
-    </div>
-</div> */}
+
+
+//Fetching Items by Category
+let abc = document.getElementById("abc");
+categories.addEventListener('change', () => {
+    if (categories.value == 0){
+        items_container.innerHTML = "";
+        
+        axios({
+            method: 'GET',
+            url: "http://127.0.0.1:8000/api/items"
+        })
+        .then(function(response){
+            var all_items = response.data["items"];
+            console.log(all_items);
+            items_container.innerHTML = "";
+            for (var i=0; i < all_items.length; i++){
+                var item_card = document.createElement("div");
+                item_card.id = "item-container";
+                item_card.innerHTML = `<div id="item">
+                                            <img src="data:image/png;base64,${all_items[i]["image"]}" alt="">
+                                            <button>❤</button>
+                                            <label id="item-title">${all_items[i]["name"]}</label>
+                                            <label id="price">${all_items[i]["price"]}$</label>
+                                        </div>`;
+                items_container.appendChild(item_card);
+            }
+        })
+
+    } else {
+
+    let data = new FormData;
+    data.append('category_id', categories.value)
+    axios({
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/getItemByCategoryId',
+        data:data,
+    })
+    .then(function(response){
+        console.log(response.data["items"]);
+        var cat_items = response.data["items"];
+        items_container.innerHTML = "";
+        for (var i=0; i < cat_items.length; i++){
+            var item_card = document.createElement("div");
+            item_card.id = "item-container";
+            item_card.innerHTML = `<div id="item">
+                                        <img src="data:image/png;base64,${cat_items[i]["image"]}" alt="">
+                                        <button>❤</button>
+                                        <label id="item-title">${cat_items[i]["name"]}</label>
+                                        <label id="price">${cat_items[i]["price"]}$</label>
+                                    </div>`;
+            items_container.appendChild(item_card);
+        }
+    })
+    }
+})
+
+
